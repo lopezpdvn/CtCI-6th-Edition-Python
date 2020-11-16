7eacb3efcf54d6804212d9ca96796066d70106b4
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-11-16 17:18:45 -0600

    solve

--- Chapter8/88WIP.py
+++ Chapter8/88WIP.py
@@ -3,17 +3,35 @@ from collections import Counter
 def f(s):
     mset = Counter(s)
     ans = []
-    g(s, '', mset, len(s), ans)
+    g('', mset, len(s), ans)
     return ans
 
-def g(s, prefix, mset, remaining, ans):
+def g(prefix, mset, remaining, ans):
     if not remaining:
         ans.append(prefix)
+        return
 
     for c, count in mset.items():
         if not count:
             continue
 
         mset[c] = count - 1
-        g(s, prefix + c, mset, remaining - 1, ans)
+        g(prefix + c, mset, remaining - 1, ans)
         mset[c] = count
+
+y = f('aaabb')
+print('Count: {0}'.format(len(y)))
+for x in y:
+    print(x)
+
+# Count: 10
+# aaabb
+# aabab
+# aabba
+# abaab
+# ababa
+# abbaa
+# baaab
+# baaba
+# babaa
+# bbaaa
