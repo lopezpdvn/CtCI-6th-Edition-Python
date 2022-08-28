72a2291c275c9bde02c25aacfe07bd3cfd2f65f8
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-08-28 15:54:49 -0500

    Create 36AnimalShelter.py

---
+++ Chapter3/36AnimalShelter.py
@@ -0,0 +1,53 @@
+class Animal: pass
+class Dog(Animal): pass
+class Cat(Animal): pass
+
+class Shelther:
+    def __init__(self):
+        from collections import deque
+        self.cats = deque()
+        self.dogs = deque()
+        self.count_anml = 0
+        self.i_anml = 0
+
+    def enqueue(self, animal):
+        if isinstance(animal, Dog):
+            self.dogs.append((self.i_anml, animal))
+        else:
+            self.cats.append((self.i_anml, animal))
+        self.i_anml += 1
+        self.count_anml += 1
+        return self.count_anml
+
+    def dequeue(self):
+        if not self.count_anml: raise Exception()
+        if not self.cats:
+            animal = self.dogs.popleft()[1]
+        elif not self.dogs:
+            animal = self.cats.popleft()[1]
+        else:
+            animal = (self.dogs.popleft()[1]
+                if self.dogs[0][0] < self.cats[0][0]
+                else self.cats.popleft()[1])
+        self.count_anml -= 1
+        return animal
+
+    def dequeue_dog(self):
+        anml = self.dogs.popleft()[1]
+        self.count_anml -= 1
+        return anml
+
+    def dequeue_cat(self):
+        anml = self.cats.popleft()[1]
+        self.count_anml -= 1
+        return anml
+
+shelter = Shelther()
+assert shelter.enqueue(Dog()) == 1
+assert shelter.enqueue(Dog()) == 2
+assert shelter.enqueue(Cat()) == 3
+assert shelter.enqueue(Dog()) == 4
+assert isinstance(shelter.dequeue(), Dog)
+assert isinstance(shelter.dequeue(), Dog)
+assert isinstance(shelter.dequeue(), Cat)
+assert isinstance(shelter.dequeue(), Dog)
