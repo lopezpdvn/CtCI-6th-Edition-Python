fca10c4b1bb28ff05d2acde487788811c6fd8503
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-09-28 08:15:07 -0700

    Update 46Successor.py

--- Chapter4/46Successor.py
+++ Chapter4/46Successor.py
@@ -1,23 +1,14 @@
-def f(p):
-    if not p:
-        return p
+def f(x):
+    if not x:
+        return None
 
-    if not p.parent or p.right:
-        return getLeftMost(p.right)
+    if x.right:
+        x = x.right
+        while x.left:
+            x = x.left
+        return x
 
-    ancs = p.parent
-    desc = p
-    while ancs and ancs.left != desc:
-        desc = ancs
-        ancs = ancs.parent
+    while x.parent and x.parent.left != x:
+        x = x.parent
 
-    return ancs
-
-def getLeftMost(p):
-    if not p:
-        return p
-
-    while p.left:
-        p = p.left
-
-    return p
+    return x.parent
