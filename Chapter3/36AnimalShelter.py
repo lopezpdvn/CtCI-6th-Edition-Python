f0eefab7ffba3e305ce403ead2cf9fb273940325
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-12-07 20:53:59 -0800

    Update 36AnimalShelter.py

--- Chapter3/36AnimalShelter.py
+++ Chapter3/36AnimalShelter.py
@@ -3,44 +3,40 @@ class Dog(Animal): pass
 class Cat(Animal): pass
 
 class Shelter:
-    def __init__(self):
-        from collections import deque
-        self.cats = deque()
-        self.dogs = deque()
-        self.count_anml = 0
-        self.i_anml = 0
+  def __init__(self):
+    from collections import deque
+    self.cats = deque()
+    self.dogs = deque()
+    self.i_anml = 0
 
-    def enqueue(self, animal):
-        if isinstance(animal, Dog):
-            self.dogs.append((self.i_anml, animal))
-        else:
-            self.cats.append((self.i_anml, animal))
-        self.i_anml += 1
-        self.count_anml += 1
-        return self.count_anml
+  def enqueue(self, animal):
+    if isinstance(animal, Dog):
+      self.dogs.append((self.i_anml, animal))
+    else:
+      self.cats.append((self.i_anml, animal))
+    self.i_anml += 1
+    return len(self)
 
-    def dequeue(self):
-        if not self.count_anml: raise Exception()
-        if not self.cats:
-            animal = self.dogs.popleft()[1]
-        elif not self.dogs:
-            animal = self.cats.popleft()[1]
-        else:
-            animal = (self.dogs.popleft()[1]
-                if self.dogs[0][0] < self.cats[0][0]
-                else self.cats.popleft()[1])
-        self.count_anml -= 1
-        return animal
+  def dequeue(self):
+    if not self: raise Exception()
+    if not self.cats:
+      animal = self.dogs.popleft()[1]
+    elif not self.dogs:
+      animal = self.cats.popleft()[1]
+    else:
+      animal = (self.dogs.popleft()[1]
+        if self.dogs[0][0] < self.cats[0][0]
+        else self.cats.popleft()[1])
+    return animal
 
-    def dequeue_dog(self):
-        anml = self.dogs.popleft()[1]
-        self.count_anml -= 1
-        return anml
+  def dequeue_dog(self):
+    return self.dogs.popleft()[1]
 
-    def dequeue_cat(self):
-        anml = self.cats.popleft()[1]
-        self.count_anml -= 1
-        return anml
+  def dequeue_cat(self):
+    return self.cats.popleft()[1]
+
+  def __len__(self):
+    return len(self.cats) + len(self.dogs)
 
 shelter = Shelter()
 assert shelter.enqueue(Dog()) == 1
