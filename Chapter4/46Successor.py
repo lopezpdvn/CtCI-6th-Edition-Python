c1e4be836a9903373616ffc15c6cd211ecc1ff70
Pedro Ivan Lopez <m@pedroivanlopez.com>
2023-04-10 08:49:21 -0700

    Update 46Successor.py

--- Chapter4/46Successor.py
+++ Chapter4/46Successor.py
@@ -1,14 +1,32 @@
-def f(x):
-    if not x:
+# Time O(height)
+# Space O(1)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def __str__(self):
+        return str(self.value)
+
+def f(node):
+    if not node: return node
+
+    if node.right:
+        return get_left_most(node.right)
+
+    while node and node.parent and node == node.parent.right:
+        node = node.parent
+
+    print(node.parent)
+    return node.parent if node else None
+
+def get_left_most(node):
+    if not node:
         return None
 
-    if x.right:
-        x = x.right
-        while x.left:
-            x = x.left
-        return x
+    while node.left:
+        node = node.left
 
-    while x.parent and x.parent.left != x:
-        x = x.parent
-
-    return x.parent
+    return node
