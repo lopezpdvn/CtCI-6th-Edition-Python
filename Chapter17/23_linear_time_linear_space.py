dcefa74c7f03145a3f7560e1f0595f86dbe6b212
Pedro Ivan Lopez <m@pedroivanlopez.com>
2023-06-09 17:12:23 -0700

    Update 23_linear_time_linear_space.py

--- Chapter17/23_linear_time_linear_space.py
+++ Chapter17/23_linear_time_linear_space.py
@@ -1,21 +1,19 @@
 def f(A):
   A = A or [[]]
   max_eq_dim = 0
-  if not A or not A[0]:
-    return max_eq_dim
-
+  if not A or not A[0]: return max_eq_dim
   B = get_B(A)
-  ans_e = None
-  irow = None
-  icol = None
 
   for i in range(len(A)):
     for j in range(len(A[0])):
-      value, row_eq, col_eq = B[i][j]
-      if value:
-        continue
-      imax_eq_dim = min(row_eq, col_eq)
-      if imax_eq_dim <= max_eq_dim:
+      left, up = B[i][j]
+      if up != left: continue
+      imax_eq_dim = up
+      down = B[i + imax_eq_dim - 1][j][1]
+      right = B[i][j + imax_eq_dim - 1][0]
+      if (down != imax_eq_dim or
+        right != imax_eq_dim or
+        imax_eq_dim <= max_eq_dim):
         continue
       max_eq_dim = imax_eq_dim
       irow = i
@@ -25,30 +23,22 @@ def f(A):
     for row in A[irow : irow + max_eq_dim]]
 
 
-def get_B(A):
-  m = len(A)
-  n = len(A[0])
-  B = [[None] * n] * m
+def get_B(A, value=0):
+  n = len(A)
+  B = [[None for i in range(n)] for j in range(n)]
 
-  for i in range(m - 1, -1, -1):
+  for i in range(n - 1, -1, -1):
     for j in range(n - 1, -1, -1):
-      value = A[i][j]
-      row_eq = None
-      col_eq = None
-      if j == n - 1 or A[i][j] != A[i][j + 1]:
-        col_eq = 1
-      else:
-        col_eq = B[i][j + 1][2] + 1
-
-      if i == m - 1 or A[i][j] != A[i + 1][j]:
-        row_eq = 1
-      else:
-        row_eq = B[i + 1][j][1] + 1
-
-      B[i][j] = (value, row_eq, col_eq)
+      if A[i][j] != value:
+        B[i][j] = [0, 0]
+        continue
+      B[i][j] = [1, 1]
+      if j + 1 < n:
+        B[i][j][1] += B[i][j + 1][1]
+      if i + 1 < n:
+        B[i][j][0] += B[i + 1][j][0]
 
   return B
-
 
 assert f([[0,0,0,0],
           [0,0,0,0],
