92e43ed37b98e632755eaec09ad3e0211e598927
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-05-19 13:43:32 -0700

    Update 22_word_transformer_recursion.py

--- Chapter17/22_word_transformer_recursion.py
+++ Chapter17/22_word_transformer_recursion.py
@@ -3,51 +3,50 @@
 # M is the cardinality of the character set
 
 def f(src, dst, words):
+  src, dst, words = (
+    src or '', dst or '', words or ())
   if not words: return ()
-  is_valid, nbor1chr = get_nbor1chr(
-            words, src, dst)
+  is_valid, G = build_graph(words, src, dst)
   if not is_valid: return ()
   from collections import deque
   path, visited = deque(), set()
-  g(dst, nbor1chr, path, visited, src)
+  g(dst, G, path, visited, src)
   return (*path,)
 
-def g(dst, nbor1chr, path, visited, src):
+def g(dst, G, path, visited, src):
   visited.add(src)
   if src == dst:
     path.appendleft(src)
     return True
 
-  for nbor in get_nbors(nbor1chr, visited, src):
-    x = g(dst, nbor1chr, path, visited, nbor)
-    if not x: continue
-    path.appendleft(src)
-    return True
+  for nbor in get_nbors(G, visited, src):
+    if g(dst, G, path, visited, nbor):
+      path.appendleft(src)
+      return True
 
   return False
 
-def get_nbors(nbor1chr, visited, x):
+def get_nbors(G, visited, x):
   n = len(x)
   for i in range(n):
     key = f'{x[:i]}*{x[i + 1:]}'
-    for nbor in nbor1chr[key]:
+    for nbor in G[key]:
       if nbor not in visited:
         yield nbor
 
 
-def get_nbor1chr(words, src, dst):
-  seen_src = False; seen_dst = False
+def build_graph(words, src, dst):
+  seen_src, seen_dst = False, False
   from collections import defaultdict
-  nbor1chr = defaultdict(set)
-  n = len(words[0])
+  G = defaultdict(set)
   for word in words:
     seen_src |= word == src
     seen_dst |= word == dst
-    for i in range(n):
+    for i in range(len(word)):
       key = f'{word[:i]}*{word[i + 1:]}'
-      nbor1chr[key].add(word)
+      G[key].add(word)
 
-  return (seen_src and seen_dst, nbor1chr)
+  return (seen_src and seen_dst, G)
 
 assert f('DAMP', 'DAMP', ()) == ()
 assert f('DAMP', 'DAMP', ('DAMP',)) == ('DAMP',)
@@ -59,4 +58,4 @@ assert f('DAMP', 'LIKE', (
 
 # Time: NL + (M^L)*(M-1)*L
 #       where NL >= M^L
-# Space: M^L + M^L)*(M-1)*L
+# Space: (M^L)*(M-1)*L
