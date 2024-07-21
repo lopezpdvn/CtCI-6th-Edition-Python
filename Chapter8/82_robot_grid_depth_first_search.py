8bbadcc8d3916ae4f26388f55e481a17f312821b
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-07-20 23:19:13 -0700

    Update 82_robot_grid_depth_first_search.py

--- Chapter8/82_robot_grid_depth_first_search.py
+++ Chapter8/82_robot_grid_depth_first_search.py
@@ -1,39 +1,20 @@
-def g(maze):
-    from collections import deque
-    path = deque()
-    if not maze or not maze[0]:
-        return path
-    m = len(maze)
-    n = len(maze[0])
-    h(maze, path, (m - 1, n - 1), (0, 0))
-    return path
+def g(A):
+  from collections import deque
+  seq = deque()
+  if (   not A or not A[0] or not A[-1][-1]
+      or not h(A, seq, 0, 0)):
+    return deque()
+  return seq
 
-def h(maze, path, target, root):
-    if root[0] > target[0] or root[1] > target[1]:
-        return False
-    if not maze[root[0]][root[1]]:
-        return False
-    if target == root:
-        path.appendleft(root)
-        return True
-    for nbori, nborj in get_nbors(maze, root):
-        found_path = h(maze, path, target,
-                       (nbori, nborj))
-        if not found_path:
-            continue
-        path.appendleft(root)
-        return True
+def h(A, seq, i, j):
+  if i == len(A) or j == len(A[0]) or not A[i][j]:
     return False
+  if ((i == len(A) - 1 and j == len(A[0]) - 1) 
+    or h(A, seq, i+1, j) or h(A, seq, i, j+1)):
+    seq.appendleft((i, j))
+    return True
 
-def get_nbors(maze, root):
-    m = len(maze)
-    n = len(maze[0])
-    if (    root[0] + 1 < m
-        and maze[root[0] + 1][root[1]]):
-        yield root[0] + 1, root[1]
-    if (    root[1] + 1 < n
-        and maze[root[0]][root[1] + 1]):
-        yield root[0], root[1] + 1
+  return False
 
 f = lambda maze: (*g(maze),)
 
