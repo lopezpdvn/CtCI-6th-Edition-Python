6699a6522dc387edc39edfc32b827bd2ad4609ed
Pedro Ivan Lopez <m@pedroivanlopez.com>
2025-06-29 16:31:54 -0700

    Update 48LCA_check_ancestor_if_needed.py

--- Chapter4/48LCA_check_ancestor_if_needed.py
+++ Chapter4/48LCA_check_ancestor_if_needed.py
@@ -1,38 +1,24 @@
 def f(x, a, b):
     if not x:
-        if not a and not b:
-            return x
-        else:
-            raise Exception()
-    
+        if not a and not b: return x
+        else: raise Exception()
     if a and not b:
         lca = get_node(x, a)
-        if lca:
-            return lca
-        else:
-            raise Exception()
-
+        if lca: return lca
+        else: raise Exception()
     if b and not a:
         lca = get_node(x, b)
-        if lca:
-            return lca
-        else:
-            raise Exception()
-
-    if not a and not b:
-        return deepest_leaf(x)
+        if lca: return lca
+        else: raise Exception()
+    if not a and not b: return deepest_leaf(x)
 
     seen = {'a': None, 'b': None}
     lca = get_lca(seen, a, b, x)
-
     if not lca: raise Exception()
     seen['a'] = seen['a'] if seen['a'] else get_node(x, a)
     seen['b'] = seen['b'] if seen['b'] else get_node(x, b)
-    if seen['a'] and seen['b']:
-        return lca
-
+    if seen['a'] and seen['b']: return lca
     raise Exception()
-
 
 def get_lca(seen, a, b, x):
     if not x: return x
@@ -42,9 +28,7 @@ def get_lca(seen, a, b, x):
     if x == b:
         seen['b'] = b
         return x
-
     llca = get_lca(seen, a, b, x.left)
     rlca = get_lca(seen, a, b, x.right)
     if llca and rlca: return x
-
-    return llca if llca else rlca
+    return llca or rlca
